e61419e86987f502ec463ca42af54c42cdae8857
Robert Verrill <robert.verrill@english.op.org>
2025-09-10 14:44:05 +0000

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 # Define variables
 PROD_SERVER="rwvagatha.duckdns.org"  # Replace with your server's IP or hostname
-PROD_DIR="/home/robert/agatha"  # Directory on production server
-LOCAL_DIR="/home/robert/agathaDev/Agatha7dev"  # Local directory with your app files
+PROD_DIR="/home/rverrill/agatha"  # Directory on production server
+LOCAL_DIR="/home/robert/agatha"  # Local directory with your app files
 IMAGE_NAME="rwv1001/agatha"  # Replace with your Docker Hub username and app name
 IMAGE_TAG=$(git rev-parse --short HEAD) # Or use a specific version tag, e.g., "v1.0"
 
@@ -13,10 +13,11 @@ docker build -t $IMAGE_NAME:$IMAGE_TAG -f production.Dockerfile .
 # Push the image to Docker Hub
 docker push $IMAGE_NAME:$IMAGE_TAG
 
-scp docker-compose-pull.yml "$PROD_SERVER:$PROD_DIR"
+scp -P 22222 docker-compose-pull.yml "$PROD_SERVER:$PROD_DIR"
 
 sed -i "s/^ENV_IMAGE_TAG=.*/ENV_IMAGE_TAG=${IMAGE_TAG}/" .env
 
-scp .env "$PROD_SERVER:$PROD_DIR"
+scp -P 22222 .env "$PROD_SERVER:$PROD_DIR"
 
-ssh "$PROD_SERVER" "docker pull $IMAGE_NAME:$IMAGE_TAG && cd $PROD_DIR && docker compose -f docker-compose-pull.yml down -v && docker compose -f docker-compose-pull.yml up -d --build"
+ssh -p 22222 "$PROD_SERVER" "docker pull $IMAGE_NAME:$IMAGE_TAG && cd $PROD_DIR && docker compose -f docker-compose-pull.yml down -v && docker compose -f docker-compose-pull.yml up -d --build && sleep 20 && ./dbrestore.sh"
+
